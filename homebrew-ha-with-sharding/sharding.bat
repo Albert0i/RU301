f4b02cdda2a5631cbe44b1890087d576d38b8df7
Alberto Iong <albert0i@hotmail.com>
2024-05-31 14:12:32 +0800

    add cluster docker

--- homebrew-ha-with-sharding/sharding.bat
+++ homebrew-ha-with-sharding/sharding.bat
@@ -8,7 +8,7 @@ start "7004"    /MIN redis-server ./conf/7004/redis.conf
 start "7005"    /MIN redis-server ./conf/7005/redis.conf
 
 
-redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 --cluster-replicas 1 --cluster-yes
+rem redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 --cluster-replicas 1 --cluster-yes
 
 rem redis-cli -p 7000 cluster nodes
 rem redis-cli -p 7000 cluster slots
