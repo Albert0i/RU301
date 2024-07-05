788741df8b25416472fd2c694ff9e1a595552731
Alberto Iong <albert0i@hotmail.com>
2024-07-05 09:17:19 +0800

    minor .bat fix

--- homebrew-ha-with-sentinels/basic-replica.bat
+++ homebrew-ha-with-sentinels/basic-replica.bat
@@ -1,4 +1,6 @@
 @echo off
+@copy /Y /V conf.bak conf > nul 
+
 start "primary"    /MIN redis-server ./conf/primary.conf 
 start "replica 1"  /MIN redis-server ./conf/replica1.conf 
 start "replica 2"  /MIN redis-server ./conf/replica2.conf 
